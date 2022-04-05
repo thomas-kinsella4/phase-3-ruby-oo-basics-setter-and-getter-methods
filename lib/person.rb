4dd10c59886d82f44cb2b2f7a81a7b31c71add3f
Thomas Kinsella <thomasjkinsella4@gmail.com>
2022-04-05 10:11:21 -0400

    commit tree aa550a4329fc51205a77c4055198a357705a26e0

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
-class Person
+require "pry"
 
+class Person
+    def name=(person_name)
+        @name = person_name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(person_job)
+        @job = person_job
+    end
+
+    def job
+        @job
+    end
 end
+
+new_person = Person.new
+
